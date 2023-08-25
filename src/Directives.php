fbcec62e5d09194b45f1764c619338a8e5cbe554
githubaction009 <140196313+githubaction009@users.noreply.github.com>
2023-08-25 23:58:21 +0600

    fix : initial loading

--- src/Directives.php
+++ src/Directives.php
@@ -2,21 +2,21 @@
 
 namespace NogorSolutionsLTD\Directives;
 
-use Illuminate\Support\Str;
-
 return [
 
-    'istrue' => function ($expression) {
-        if (Str::contains($expression, ',')) {
-            $expression = Parser::multipleArgs($expression);
+    'routeis' => function ($expression) {
+        return "<?php if (fnmatch({$expression}, Route::currentRouteName())) : ?>";
+    },
 
-            return implode('', [
-                "<?php if (isset({$expression->get(0)}) && (bool) {$expression->get(0)} === true) : ?>",
-                "<?php echo {$expression->get(1)}; ?>",
-                '<?php endif; ?>',
-            ]);
-        }
+    'endrouteis' => function ($expression) {
+        return '<?php endif; ?>';
+    },
 
-        return "<?php if (isset({$expression}) && (bool) {$expression} === true) : ?>";
+    'routeisnot' => function ($expression) {
+        return "<?php if (! fnmatch({$expression}, Route::currentRouteName())) : ?>";
+    },
+
+    'endrouteisnot' => function ($expression) {
+        return '<?php endif; ?>';
     },
 ];
